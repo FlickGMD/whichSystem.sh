38ba99c4981275844fee011afa536cc7345a27ea
FlickGMD <salvadorfabiansulcacuba@gmail.com>
2025-01-03 22:12:20 +0000

    Nuevos cambios

--- whichSystem.sh
+++ whichSystem.sh
@@ -1,45 +1,27 @@
 #!/bin/bash
 
-# Autor: Flick
+# \\ Colours
+greenColour="\e[0;32m\033[1m" endColour="\033[0m\e[0m" redColour="\e[0;31m\033[1m" purpleColour="\e[0;35m\033[1m" grayColour="\e[0;37m\033[1m"
 
-# Colours
+ip=$1
 
-greenColour="\e[0;32m\033[1m"
-endColour="\033[0m\e[0m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
-grayColour="\e[0;37m\033[1m"
-
-function ctrl_c(){
-  echo -e "\n${redColour}[!] Saliendo...${endColour}\n\n"
-  tput cnorm; exit 1
-}
-trap ctrl_c INT
-
-# Variables globales
-IpAdress=$1
-
-if [[ "$IpAdress" =~ [a-zA-Z] ]]; then
-  echo -e "\n${redColour}[!] Las direcciones IP no contienen caracteres alfabeticos.${endColour}"
-  exit 1
+if [[ ! $ip ]]; then # \\ Si no le pasamos argumentos, saldremos del script
+  echo -e "\n\n${redColour}[!]${endColour} ${grayColour}Uso:${endColour} ${purpleColour}$0${grayColour} <ip_address>${endColour}\n"
+  exit 
 fi
-tput civis
-if [[ "$IpAdress" ]]; then
-  ttl=$(/usr/bin/ping -c 1 "$IpAdress" 2>/dev/null | grep "ttl" | awk '{print $6}' | sed 's/=/ /g' | awk 'NF{print $NF}')
-  if [[ -z $ttl ]]; then 
-    echo -e "\n${redColour}[!] ERROR: ${endColour} ${grayColour}No hay una respuesta por parte de la IP $IpAdress${endColour}\n"
-    tput cnorm; exit 1
-  fi
-    if [[ $ttl -le 64 ]] && [[ $ttl -ge 0 ]]; then 
-        echo -e "\n${purpleColour}$IpAdress${endColour}${grayColour} (ttl -> $ttl):${endColour} ${greenColour}Linux${endColour}\n"
-        tput cnorm
-    elif [[ $ttl -ge 65 ]] && [[ $ttl -le 128 ]]; then
-     echo -e "\n${purpleColour}$IpAdress${endColour}${grayColour} (ttl -> $ttl):${endColour} ${greenColour}Windows${endColour}\n"
-     tput cnorm 
-    fi 
+
+# \\ Variables
+ttl=$(ping -c 1 $ip | grep -oP 'ttl=\d{1,3}' | cut -d '=' -f2)
+ip_address=$(ping -c 1 $ip | grep -oP '(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})' | awk 'NR==1')
+
+if [[ $ttl -gt 0 && $ttl -le 64 ]]; then # \\ Mayor igual a 0, menor igual a 64 -> Linux
+  echo -e "\n\n${purpleColour}$ip_address${endColour} ${grayColour}(ttl -> $ttl):${endColour} ${greenColour}Linux${endColour}\n"
+
+elif [[ $ttl -ge 65 && $ttl -le 128 ]]; then # \\ Mayor igual a 65, menor igual a 128 -> Windows
+  echo -e "\n\n${purpleColour}$ip_address${endColour}${grayColour} (ttl -> $ttl):${endColour}${greenColour} Windows${endColour}\n"
+
 else
-tput cnorm; echo -e "\n${redColour}[!]${endColour} ${grayColour}Uso:${endColour} ${purpleColour}$0${endColour} ${grayColour}<Dirección_IP>${endColour}\n"
+  echo -e "\n\n${purpleColour}$ip_address ${grayColour}(ttl -> $ttl):${endColour} ${redColour}Not found.${endColour}\n" # \\ Caso contrario, de que ninguno sea verdadero
+
 fi
+
